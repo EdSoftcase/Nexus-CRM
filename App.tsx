488fc275ff82d0ab485ca9ad0b9e5841b75f1651
EdSoftcase <edson.softcase@gmail.com>
2025-12-07 22:46:44 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { DataProvider, useData } from './context/DataContext'; 
